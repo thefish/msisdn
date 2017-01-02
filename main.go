b745129633d01a3d7445087d21bbe5229ce71546
D.B <thejan.2009@gmail.com>
2017-01-02 14:58:57 +0100

    add ndc parsing for slovenian numbers

--- main.go
+++ main.go
@@ -2,13 +2,18 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"log"
 	"strings"
 )
 
 func main() {
-	parse("38631123456")
-	parse("8801812345678")
+	for _, i := range []string{"38631123456", "38670987654", "8801812345678"} {
+		err := parseMsisdn(i)
+		if err != nil {
+			log.Println(err)
+		}
+	}
 }
 
 var countryCodes = map[string]string{
@@ -17,24 +22,63 @@ var countryCodes = map[string]string{
 	"880": "BD",
 }
 
-func parse(msisdn string) error {
+type ndcMno struct {
+	ndc, mno string
+}
+
+var countryNDCs = map[string][]ndcMno{
+	"SI": []ndcMno{
+		ndcMno{"31", "Mobitel"},
+		ndcMno{"41", "Mobitel"},
+		ndcMno{"51", "Mobitel"},
+		ndcMno{"71", "Mobitel"},
+		ndcMno{"30", "Si.mobil"},
+		ndcMno{"40", "Si.mobil"},
+		ndcMno{"68", "Si.mobil"},
+		ndcMno{"70", "Telemach"},
+		ndcMno{"64", "T-2"},
+	},
+}
+
+func parseMsisdn(in string) error {
 
 	// assume that input is valid - just numbers
-	if len(msisdn) > 15 {
+	if len(in) > 15 {
 		return errors.New("Input too long.")
 	}
 
 	var isoID, cdc string
 	for code, country := range countryCodes {
-		if strings.HasPrefix(msisdn, code) {
+		if strings.HasPrefix(in, code) {
 			cdc = code
 			isoID = country
-			msisdn = strings.TrimLeft(msisdn, code)
+			in = strings.TrimPrefix(in, code)
 			break
 		}
 	}
 
-	log.Println("cdc:", cdc, "country id:", isoID, "ndc+sn:", msisdn)
+	if cdc == "" {
+		return errors.New("Unknown Country Code.")
+	}
+
+	var mno string
+	if countryData, ok := countryNDCs[isoID]; ok {
+		for _, v := range countryData {
+			if strings.HasPrefix(in, v.ndc) {
+				mno = v.mno
+				in = strings.TrimPrefix(in, v.ndc)
+				break
+			}
+		}
+	} else {
+		return fmt.Errorf("Country \"%s\" not implemented", isoID)
+	}
+
+	if mno == "" {
+		return errors.New("Network Destination Code unknown")
+	}
+
+	log.Println("cdc:", cdc, "country id:", isoID, "mno:", mno, "sn:", in)
 
 	return nil
 }
